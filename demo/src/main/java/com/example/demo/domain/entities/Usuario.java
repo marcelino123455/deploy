992b24ae337b74859dbe6ca8aeac545b38c2369f
Juan Leibniz Aquino Espinoza <you@example.com>
2023-10-26 15:44:50 -0500

    inicio

--- demo/src/main/java/com/example/demo/domain/entities/Usuario.java
+++ demo/src/main/java/com/example/demo/domain/entities/Usuario.java
@@ -1,5 +1,42 @@
 package com.example.demo.domain.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
+@Table(name = "usuario")
 public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+    private String email;
+    private String dni;
+    private String contrasenia;
+    private String puntos;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Comic> comic_posteado = new ArrayList<>();
+    
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<Comentarios> comentarios = new HashSet<>();
+
     
 }
---
+++ demo/src/main/java/com/example/demo/infraestructure/usuarioRepository.java
@@ -0,0 +1,10 @@
+package com.example.demo.infraestructure;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.domain.entities.Usuario;
+
+
+public interface usuarioRepository extends JpaRepository<Usuario, Long> {}
+
+
