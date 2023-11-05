0facbac3c034568431490ad346bb11d56885b106
Juan Leibniz Aquino Espinoza <you@example.com>
2023-11-05 17:27:42 -0500

    autentificación arreglada

--- demo/src/main/java/com/example/demo/Authentification/user/SigninRequest.java
+++ demo/src/main/java/com/example/demo/Authentification/user/SigninRequest.java
@@ -1,30 +1,15 @@
 package com.example.demo.Authentification.user;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class SigninRequest {
     private String username;
     private String password;
-
-    public SigninRequest() {
-    }
-
-    public SigninRequest(String username, String password) {
-        this.username = username;
-        this.password = password;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
--- demo/src/main/java/com/example/demo/infraestructure/usuarioRepository.java
+++ demo/src/main/java/com/example/demo/infraestructure/usuarioRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.domain.entities.Usuario;
 
-
 public interface usuarioRepository extends JpaRepository<Usuario, Long> {
     Usuario findByNombre(String nombre);
 }
